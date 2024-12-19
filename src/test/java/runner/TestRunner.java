0e8989c63bc71a5da1a33de4ef0bcfa32983ceea
Siddik-1997 <abubakkar1045@gmail.com>
2024-12-19 23:24:45 +0600

    Some code updated

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -11,7 +11,13 @@ public class TestRunner extends Setup {
     LoginPage loginPage;
     ApplyLeavePage applyLeavePage;
 
-    @Test(priority = 1, description = "Verify Login with valid credential")
+    @Test(priority = 1, description = "Verify login functionality doesn't works with invalid credentials")
+    public void doLoginWithInvalidCred() {
+        loginPage = new LoginPage(driver);
+        loginPage.doLogin("Admin", "admin10203");
+    }
+
+    @Test(priority = 2, description = "Verify login functionality works properly with valid credentials")
     public void doLogin() {
         loginPage = new LoginPage(driver);
         loginPage.doLogin("Admin", "admin123");
@@ -22,7 +28,7 @@ public class TestRunner extends Setup {
         Assert.assertEquals(headerActualText, headerExpectedText);
     }
 
-    @Test(priority = 2, description = "Verify user redirects to the Apply Leave page correctly")
+    @Test(priority = 3, description = "Verify if user is redirected to the Apply Leave page correctly.")
     public void redirectApplyLeavePage() throws InterruptedException {
         applyLeavePage = new ApplyLeavePage(driver);
         applyLeavePage.redirectToApplyLeavePage();
@@ -33,7 +39,7 @@ public class TestRunner extends Setup {
         Assert.assertEquals(header_actual, header_expected);
     }
 
-    @Test(priority = 3, description = "Verify user can not apply leave with selecting an invalid date")
+    @Test(priority = 4, description = "Verify user cannot apply for leave with an invalid date.")
     public void cantApplyLeaveWithoutValidDate() throws InterruptedException {
         applyLeavePage = new ApplyLeavePage(driver);
         applyLeavePage.oneDayLeave("20-20-12", "2024-28-2022", "Apply with an invalid date");
@@ -45,32 +51,32 @@ public class TestRunner extends Setup {
         Assert.assertEquals(headerActualText, headerExpectedText);
     }
 
-    @Test(priority = 4, description = "Verify user can not apply leave on Vacation/Off Date")
+    @Test(priority = 5, description = "Verify that leave cannot be applied on Vacation or Off dates.")
     public void cantApplyLeave() throws InterruptedException {
         applyLeavePage = new ApplyLeavePage(driver);
         applyLeavePage.oneDayLeave("2024-25-12", "2024-28-2022", "Apply leave request on vacation/Off date");
         Thread.sleep(200);
     }
 
-    @Test(priority = 5, description = "Verify user can successfully apply leave for one day")
+    @Test(priority = 6, description = "Verify that user can successfully apply for leave for a single day.")
     public void applyOneDayLeave() throws InterruptedException {
         applyLeavePage = new ApplyLeavePage(driver);
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/leave/applyLeave");
         Thread.sleep(2000);
-        applyLeavePage.oneDayLeave("2024-27-12", "2024-27-12", "Apply Sick Leave for one day");
+        applyLeavePage.oneDayLeave("2024-27-12", "2024-27-12", "Apply Sick Leave for single day");
         Thread.sleep(200);
     }
 
-    @Test(priority = 6, description = "Verify user can successfully apply leave for more days")
+    @Test(priority = 7, description = "Verify that user can successfully apply for leave for multiple days.")
     public void applyMoreDayLeave() throws InterruptedException {
         applyLeavePage = new ApplyLeavePage(driver);
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/leave/applyLeave");
         Thread.sleep(2000);
-        applyLeavePage.oneDayLeave("2024-28-12", "2024-30-12", "Apply Sick Leave for more days");
+        applyLeavePage.oneDayLeave("2024-28-12", "2024-30-12", "Apply Sick Leave for multiple days");
         Thread.sleep(200);
     }
 
-    @Test(priority = 7, description = "Verify user can not apply same leave Request twice")
+    @Test(priority = 8, description = "Verify that user cannot apply for the same leave request twice.")
     public void applyLeaveTwice() throws InterruptedException {
         applyLeavePage = new ApplyLeavePage(driver);
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/leave/applyLeave");
@@ -78,7 +84,7 @@ public class TestRunner extends Setup {
         Thread.sleep(200);
     }
 
-    @Test(priority = 8, description = "Verify user can not apply if he exceeds the leave balance")
+    @Test(priority = 9, description = " Verify that system restricts leave applications that exceed the user’s leave balance.")
     public void exceedsApplyLeave() throws InterruptedException {
         applyLeavePage = new ApplyLeavePage(driver);
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/leave/applyLeave");
@@ -86,7 +92,7 @@ public class TestRunner extends Setup {
         Thread.sleep(200);
     }
 
-    @Test(priority = 10, description = "Verify user can Logout Successfully")
+    @Test(priority = 10, description = "Verify that user can log out successfully.")
     public void logOut() {
         loginPage = new LoginPage(driver);
         loginPage.doLogout();
